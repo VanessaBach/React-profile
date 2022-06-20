dff497f73653ee7318fcee213d1a4f37385a643a
vanessabach <vanessabach.r@gmail.com>
2022-06-20 18:10:42 -0300

    sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import {PencilLine} from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover} 
+        src="https://images.unsplash.com/photo-1589652717521-10c0d092dea9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40"  
+      />
+      
+      <div className={styles.profile}>
+        <img  className={styles.avatar} src="https://avatars.githubusercontent.com/u/9847731?v=4" />
+
+        <strong>Vanessa Bach</strong>
+        <span>Web Developer</span>
+        <footer>
+          <a href="#">
+            <PencilLine size={20}/>
+            Editar seu perfil
+          </a>
+        </footer>
+
+      </div>
+    </aside>
+  );
+}
