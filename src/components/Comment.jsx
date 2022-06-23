1451b33420cf9a1062f19c90eaea15f3a6af7467
vanessabach <vanessabach.r@gmail.com>
2022-06-23 16:03:03 -0300

    creating new comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    deleteComment(content);
+  }
+
   return(
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/9847731?v=4"  alt="" />    
@@ -18,11 +22,11 @@ export function Comment() {
             </button>
 
           </header>
-          <p>Parabéns, Van!!</p>
+          <p>{content}</p>
         </div>
 
         <footer>
-          <button>
+          <button onClick={ handleDeleteComment}>
             <ThumbsUp/>
             Aplaudir <span> 20 </span>
           </button>          
