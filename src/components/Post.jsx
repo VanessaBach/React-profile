0d8027ad4442417c921c138ae744b61adc3fc747
vanessabach <vanessabach.r@gmail.com>
2022-06-24 15:01:45 -0300

    fixing setCustomValidity

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,7 +30,7 @@ export function Post({author, publishedAt, content}) {
   }
 
   function handleNewCommentChange() {
-    event.target.setCustomValidity(' ');
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
   }
 
